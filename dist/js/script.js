12510568ba993235a5f093e2adc49797ba301204
Hxnlchs <ferdinant1912@gmail.com>
2024-01-24 12:42:03 +0800

    add submit form function

--- dist/js/script.js
+++ dist/js/script.js
@@ -39,3 +39,19 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
+
+//Submit Form
+window.addEventListener("load", function () {
+  const form = document.getElementById("my-form");
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    const data = new FormData(form);
+    const action = e.target.action;
+    fetch(action, {
+      method: "POST",
+      body: data,
+    }).then(() => {
+      alert("Success!");
+    });
+  });
+});
